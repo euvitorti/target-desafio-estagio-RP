27b67211be74e15848663a640777d91a388d6bb9
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-31 15:48:39 -0300

    Somar números

--- target/src/desafioSoma/Soma.java
+++ target/src/desafioSoma/Soma.java
@@ -1,22 +1,23 @@
 package desafioSoma; // Define o pacote onde a classe Soma está localizada
 
-// DESAFIO EM QUESTÃO
+
 // Observe o trecho de código abaixo:
-// int INDICE = 13, SOMA = 0, K = 0; Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
-// Imprimir(SOMA); Ao final do processamento, qual será o valor da variável SOMA?
+// int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+// Ao final do processamento, qual será o valor da variável SOMA?
 
 public class Soma { // Declaração da classe Soma
 
     // Declaração de variáveis de instância privadas
     private int soma; // Variável que irá armazenar o valor da soma acumulada
-    private int k; // Variável auxiliar usada para incrementar a cada iteração
 
     // Método público que realiza a operação de soma
     public int somar() {
 
-        // Loop 'for' que itera de 0 a 12 (13 iterações)
-        for(int i = 0; i <= 12; i++){
-            this.k = k + 1; // Incrementa o valor de 'k' em 1 a cada iteração (k = k + 1)
+        int k = 1;
+        int i = 12;
+
+        while(k != i) {
+            k = k + 1; // Incrementa o valor de 'k' em 1 a cada iteração (k = k + 1)
             this.soma = soma + k; // Adiciona o novo valor de 'k' à variável 'soma' (soma = soma + k)
         }
 
